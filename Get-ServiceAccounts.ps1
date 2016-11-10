18fc184b1e45102db6ed40d989a3c823060106d6
jaecee <github@awesomelymodest.com>
2016-11-09 16:38:18 -0800

    whitespace

--- Get-ServiceAccounts.ps1
+++ Get-ServiceAccounts.ps1
@@ -1,5 +1,5 @@
 ﻿#Grab service accounts on all servers
-$servers = Get-ADComputer -Filter * -Property Name,OperatingSystem | Where-Object {$_.OperatingSystem -like "*server*"} |select name -ExpandProperty name
+$servers = Get-ADComputer -Filter * -Property Name,OperatingSystem | Where-Object {$_.OperatingSystem -like "*server*"} | select name -ExpandProperty name
 
 Foreach ($s in $servers){
     Get-WmiObject win32_service -computername $s | Select-Object systemname,name,startname,state | Export-Csv $name-services.txt -NoTypeInformation
