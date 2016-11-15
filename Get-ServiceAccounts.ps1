0d6785197f7cbc681b90d7f7eb762fa27be17c08
jaecee <github@awesomelymodest.com>
2016-11-15 11:16:40 -0800

    testing desktop

--- Get-ServiceAccounts.ps1
+++ Get-ServiceAccounts.ps1
@@ -1,4 +1,5 @@
-﻿#Grab service accounts on all servers
+﻿#testing
+#Grab service accounts on all servers
 $servers = Get-ADComputer -Filter * -Property Name,OperatingSystem | Where-Object {$_.OperatingSystem -like "*server*"} | select name -ExpandProperty name
 
 Foreach ($s in $servers){
