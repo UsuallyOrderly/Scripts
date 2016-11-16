dc4f78e5a3185db43b41c961cc962eafc559966a
jaecee <github@awesomelymodest.com>
2016-11-15 17:17:33 -0800

    Adding these

---
+++ ConnectToO365.ps1
@@ -0,0 +1,9 @@
+﻿   #Get O365 Credential
+   $Office365Credential = Get-Credential
+   
+   #region Connect to Office 365
+    Write-Verbose "Connecting to office 365 Configuration Microsoft.Exchange"
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Office365Credential -Authentication Basic -AllowRedirection
+    Import-PSSession $Session | Out-Null
+    Connect-MSolService –Credential $Office365Credential | Out-Null
+    #endregion Connect to Office 365
---
+++ Get-ServerDetails.ps1
@@ -0,0 +1,16 @@
+﻿#Grab Server version & GUI presence for all servers
+$servers = Get-ADComputer -Filter * -Property Name,OperatingSystem | Where-Object {$_.OperatingSystem -like "*server*"} | select name -ExpandProperty name
+$output = @()
+
+foreach ($s in $servers) {
+    $os = Get-WmiObject win32_operatingsystem -ComputerName $s
+    $gui = Get-WMIObject win32_optionalfeature -computer $s | where Name -eq 'Server-Gui-Shell' #| Select InstallState # 1 = GUI, 2 = CORE
+
+    $output += [pscustomobject]@{
+        'ComputerName' = $s
+        'OS Version' = $os.caption
+        'Service Pack' = $os.ServicePackMajorVersion
+        'InstallState' = (&{ if( $gui.installstate -eq '1') { "GUI" } else { "Core" } } )
+    }
+}
+$output #| Out-GridView
